87826f907339b696d478fecd988c4694a0573330
Harold-Noland-II <supersmashdude17@gmail.com>
2024-05-25 09:00:04 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,11 +22,14 @@ int main()
     cout << "Please enter a number:" << endl;
     cin >> input;
     hours = input % 24
-    minutes = input % (24 * 60)
-    seconds = input % (24 * 60 * 60)
+    minutes = input * (24 * 60) % (24 * 60)
+    seconds = input * (24 * 60 * 60) % (24 * 60 * 60)
     cout << "The number of hours in the number is: " << hours;
     cout << "The number of minutes in the number is: " << minutes;
     cout << "The number of seconds in the number is: " << seconds;
 }
 
 // Place any unused code here so that the instructor can follow your prblem solving thoughts.
+// hours = input / 24
+// minutes = input % (24 * 60)
+// seconds = input % (24 * 60 * 60)
